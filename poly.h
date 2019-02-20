635bad44df4b55dd22a73cac910dab86ac6d0816
Bartek <b.michalak000@op.pl>
2019-02-20 18:11:59 +0100

    Nareszcie ukonczone na godzinach przyjec

--- poly.h
+++ poly.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
@@ -9,48 +10,83 @@ class poly //klasa poly
 {
 private:
 	double* tabpoly;
-	double* newtabpoly;
 	unsigned int size;
-	
 public:
-	poly(double w=0) 
-		{		
-			tabpoly = new double[0];
-			tabpoly[0]=w;	
-		}
-	
-	
-	~poly()
-		{
-			delete [] tabpoly;
-		}
 
-	poly(const poly &p)
-		{
-			tabpoly = new double [p.size];
-			size=p.size;
-		
-			for (int i=0;i<p.size;i++)
-				tabpoly[i]=p.tabpoly[i];
-		}
 
-	double & operator[] (unsigned int nr) //przypisanie [] dostêpu do ka¿dego elementu w tablicy
+
+	poly(double w=0) //wpisanie do tablicy
 	{
-		if(tabpoly.size() <= nr)
-		{
-			for(unsigned int i=0; i<nr; ++i)
-			{
-				this->tabpoly.push_back(0);
-			}
+	
+	tabpoly = new double[1];
+	size = 1;
+
+	tabpoly[0]=w;
+	}
+
+
+~poly()
+{
+		
+	delete [] tabpoly;
+
+}
+
+poly(const poly &p)
+{
+	tabpoly = new double [p.size];
+	size=p.size;
+
+	for (unsigned int i=0;i<p.size;++i)
+		tabpoly[i]=p.tabpoly[i];
+}
+
+
+poly & operator=(const poly & p)
+{
+	double* newtabpoly;
+
+	newtabpoly = new double [p.size];
+	size = p.size;
+	
+	for (unsigned int i=0;i<p.size;i++)
+		newtabpoly[i]=p.tabpoly[i];
+
+	swap(tabpoly, newtabpoly);
+
+	delete [] newtabpoly;
+	
+	return * this;
+}
+
+
+	double & operator[] (unsigned int nr) //przypisanie [] dostÃªpu do kaÂ¿dego elementu w tablicy
+	{
+		double* newtabpoly;
+
+		if(size <= nr)
+		{	
+			
+			newtabpoly = new double [nr+1];		
+			for(unsigned int i=0; i<size; ++i)
+				newtabpoly[i]=tabpoly[i];
+			for(unsigned int j=size; j<nr+1; ++j)
+				newtabpoly[j]=0;
+			size = nr+1;
+			swap(tabpoly, newtabpoly);	
+			delete [] newtabpoly;		
 		}
-		return this->tabpoly[nr];
+
+
+		return tabpoly [nr];
+
 	};
 	
 	double operator() (double x) //funktor klasy, wartosc wielomianu w pkt
 	{
 		double wyn=0;
 		
-		for(unsigned int i=0; i< this->tabpoly.size(); ++i)
+		for(unsigned int i=0; i< size; ++i)
 		{
 			wyn += this->tabpoly[i] * pow(x, i);
 		}
@@ -58,66 +94,57 @@ public:
 		return wyn;
 	}
 
-	friend ostream & operator << (ostream & s, const poly &wie) // wyswietlanie
+	friend ostream & operator << (ostream & s, const poly &p) // wyswietlanie
 	{
-		int size = wie.tabpoly.size();
 		
-		for(unsigned int i=0; i< size; ++i)
+		for(unsigned int i=0; i<p.size; ++i)
 		{
-			if (wie.tabpoly[i]!=0)
+			if (p.tabpoly[i]!=0)
 			{
 				if(i==0) //wyswietlanie stopnia 0
 				{
-					s << wie.tabpoly[i];					
-				}	
+					s << p.tabpoly[i];					
+				}	 
 				else if(i==1)  //wyswietlanie stopnia 1
 				{
-					s << " + " << wie.tabpoly[i]<<"x";				
+					s << " + " << p.tabpoly[i]<<"x";				
 				}
 	
 				else // wyswietlanie stopnia > 1
 				{
-					
-					s << " + "<< wie.tabpoly[i]<<"x^"<< i;				
+					s << " + "<< p.tabpoly[i]<<"x^"<< i;				
 				}
 		   	}
 		}
 		return s;
 	};
 
-	poly operator+ (const poly& pol) //sumowanie wielomianow
+
+	poly operator+ (const poly& p) //sumowanie wielomianow
 	{
 		poly Poly;
 		
-		int size=0;
-		size = pol.tabpoly.size();
-
-		for(unsigned int i=0; i<this->tabpoly.size(); ++i)
-		{
+		for(unsigned int i=0; i<this->size; ++i)
 				Poly[i] = this->tabpoly[i];
-		}
 		
-		for(unsigned int i=0; i<size; ++i)
-		{
-				Poly[i] += pol.tabpoly[i];
-		}
+		for(unsigned int i=0; i<p.size; ++i)
+				Poly[i] += p.tabpoly[i];
 
 		return Poly;
 	};
-
-	friend poly operator* (const poly& poly1, const poly& poly2) //mno¿enie wielomianów 
-	{
-		poly Poly;
-		
-		for(unsigned int i=0; i<poly1.tabpoly.size(); i++)
+	friend poly operator* (const poly& p1, const poly &p2) //mnoÂ¿enie wielomianÃ³w 
 		{
-			for(unsigned int n=0; n<poly2.tabpoly.size(); n++)
-			{
-				Poly[i+n] += poly1.tabpoly[i] * poly2.tabpoly[n];
+			poly Poly;
+			for(unsigned int i=0; i<p1.size; ++i)
+			{	
+				for(unsigned int n=0; n<p2.size; n++)
+							Poly[i+n] += p2.tabpoly[n] * p1.tabpoly[i];
 			}
+
+
+			return Poly;
 		}
-		
-		return Poly;
-	};
 	
 };
+
+
